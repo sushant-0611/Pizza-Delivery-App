c3611a84daf790a02d03e0169d5a6afecfb2e3dd
Sushant Kakade <sushantkakade1111@gmail.com>
2026-07-07 23:15:08 +0530

    Create Dashboard component for admin interface

---
+++ client/src/admin/Dashboard.jsx
@@ -0,0 +1,19 @@
+function Dashboard() {
+  return (
+
+    <div>
+
+      <h2 className="fw-bold">
+        Admin Dashboard
+      </h2>
+
+      <p className="text-muted">
+        Welcome Admin 👋
+      </p>
+
+    </div>
+
+  );
+}
+
+export default Dashboard;
